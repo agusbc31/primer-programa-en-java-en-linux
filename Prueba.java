e2905196743ccd892030d06443ef7ee9ae2a1453
agusbc31 <agusbc31@gmail.com>
2023-09-27 13:35:20 +0200

    Añadida comida favorita

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,7 @@ public class Prueba{
 
         System.out.println("Barrero Casquero");
         System.out.println("19 años, 2004");
+        System.out.println("Mi comida favorita es hamburguesa");
     }
 
 

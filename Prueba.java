538e1c0c0a941863c000990d2dcec63f309d68ab
agusbc31 <agusbc31@gmail.com>
2023-10-03 13:07:38 +0200

    Artista favorito

--- Prueba.java
+++ Prueba.java
@@ -1,9 +1,8 @@
 public class Prueba{
     public static void main(String args[]) {
-
-        System.out.println("Barrero Casquero");
         System.out.println("19 años, 2004");
         System.out.println("Mi comida favorita es hamburguesa");
+        System.out.println("Mi artista favorito es Melendi");
     }
 
 
